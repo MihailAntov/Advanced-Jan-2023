794930aa819f4c3fa8c3fdb8c62f1e7f09e40f34
MihailAntov <114488260+MihailAntov@users.noreply.github.com>
2023-01-12 17:40:31 +0200

    exercises added

---
+++ Stacks and Queues - Exercise/04.FastFood/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04.FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int food = int.Parse(Console.ReadLine());
+            Queue<int> orders = new Queue<int>(Console.ReadLine()
+                .Split(" ")
+                .Select(n=>int.Parse(n)));
+
+            Console.WriteLine(orders.Max());
+
+            while(orders.Count > 0)
+            {
+                if(orders.Peek() <= food)
+                {
+                    food -= orders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+
+            if(orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+            }
+
+
+        }
+    }
+}
